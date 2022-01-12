502587996f123ae286b01d48584f6b0ad8e3f458
666CCsquele666 <77618788+666CCsquele666@users.noreply.github.com>
2022-01-12 20:09:14 +0100

    INIT ÅÅH

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import OneSignal from "react-onesignal";
 
 function App() {
+
+  useEffect(() => {
+    OneSignal.init({
+      appId: "ae615730-5a3d-41d0-95da-6a6bf663a47d",
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
